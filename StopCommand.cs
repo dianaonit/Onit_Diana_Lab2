6b2ca3519de936f60831024301fa5efb4675894a
onitd <onitd@DESKTOP-RQGU8LH>
2020-11-02 12:29:09 +0200

    Am incarcat lab4 (acum ruleaza)

--- StopCommand.cs
+++ StopCommand.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 
-namespace Onit_Diana_Lab2
+namespace Onit_Diana_Lab2.CustomCommands
 {
     class StopCommand
     {
